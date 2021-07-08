2cf800b82213336718ff7e4d536f9907d7c338aa
Adriano Matos <adrianotaua@hotmail.com>
2021-07-08 20:31:29 -0300

    Até condicionais

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-var myName = "Adriano";
+var myName = "Olá Mundo. Bem vindo Adriano";
+var idade = 35;
+var altura = 1.79;
+var ehEstudante = true;
+var vazio;
 
-let language = "JavaScript";
+/* objeto */
+var pessoa = { 
+    "name": "Matos",
+    "age": 35
+}
 
-const
+/*  array */
+var frutas = ["banana", "laranja", "uva", "maçã"]
+
+/* funcao */
+function soma(a,b){
+    return a+b;
+}
+
+/* Mostrar tipo dos dados */
+console.log(typeof "a");
+console.log(typeof 32);
+console.log(typeof name);
+console.log(typeof ehEstudante);
+console.log(typeof vazio);
+console.log(typeof soma);
+
+/* const sobrenome; */
+/* {
+    let idade = 35;
+} */
 
 console.log(myName)
+/* console.log("Idade: "+idade) */
+
+/* Existem três tipos de declarações em JavaScript.
+### VAR - Declara uma variável, opcionalmente, inicializando-a com um valor.
+### LET - Declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+### CONST- Declara uma constante de escopo de bloco, apenas de leitura. 
+Obrigatório atribuição na criação
+Proibido modificar seu valor
+*/
